06402df64156d709450cbdf48bffd7e62f5c8903
khalil el kadih <khalil94elkadih@gmail.com>
2022-11-24 11:34:03 +0100

    first commit

---
+++ Visionary__Crofting/src/main/java/com/visionarycrofting/visionary__crofting/VisionaryCroftingApplication.java
@@ -0,0 +1,14 @@
+package com.visionarycrofting.visionary__crofting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisionaryCroftingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VisionaryCroftingApplication.class, args);
+    }
+
+}
+git
---
+++ Visionary__Crofting/src/test/java/com/visionarycrofting/visionary__crofting/VisionaryCroftingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.visionarycrofting.visionary__crofting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisionaryCroftingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
