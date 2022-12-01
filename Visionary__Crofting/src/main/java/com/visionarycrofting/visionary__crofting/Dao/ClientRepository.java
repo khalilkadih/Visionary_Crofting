2bfdc7a70903a30bafafb693a857395067a3ff3a
RAWYAYAA <93930003+RAWYAYAA@users.noreply.github.com>
2022-12-01 09:30:54 +0100

    last pull

--- Visionary__Crofting/src/main/java/com/visionarycrofting/visionary__crofting/Dao/ClientRepository.java
+++ Visionary__Crofting/src/main/java/com/visionarycrofting/visionary__crofting/Dao/ClientRepository.java
@@ -3,6 +3,7 @@ package com.visionarycrofting.visionary__crofting.Dao;
 import com.visionarycrofting.visionary__crofting.Entities.Client;
 import com.visionarycrofting.visionary__crofting.Entities.Command;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
